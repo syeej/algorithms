55e2649e84f476d9b542532abff4cff8d0db9b87
yooniverse <141565053+syeej@users.noreply.github.com>
2024-01-04 18:49:16 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Set;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        //의상 종류별 개수
+		HashMap<String, Integer> map = new HashMap<>();
+		for(String[] clothType: clothes) {
+			map.put(clothType[1], map.getOrDefault(clothType[1], 0)+1);
+		}
+        //의상 조합 수
+        Set<String> keySet = map.keySet();
+		for(String key: keySet) {
+			answer *= map.get(key)+1;
+		}
+		answer -= 1; //하루에 최소 1개 의상 입음
+        return answer;
+    }
+}
