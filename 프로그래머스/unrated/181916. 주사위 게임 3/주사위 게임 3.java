b1728ee8498d51ae4b7b852413d8b3f39a44b9f1
yooniverse <141565053+syeej@users.noreply.github.com>
2024-01-07 02:22:56 +0900

    [level unrated] Title: 주사위 게임 3, Time: 0.46 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int a, int b, int c, int d) {
+        int[] arr = new int[] {a,b,c,d};
+		Arrays.sort(arr);
+        int answer = 0;
+        
+		if(arr[0] == arr[3]) {
+			answer = 1111*arr[0];
+		}else if(arr[0] == arr[2]) {
+			answer = (int)Math.pow(10*arr[0]+arr[3], 2);
+		}else if(arr[1] == arr[3]) {
+			answer = (int)Math.pow(10*arr[1]+arr[0], 2);
+		}else if(arr[0] == arr[1] && arr[2] == arr[3]) {
+			answer = (arr[0] + arr[2])*(arr[2]-arr[0]);
+		}else if(arr[0] == arr[1]) {
+			answer = arr[2]*arr[3];
+		}else if(arr[1] == arr[2]) {
+			answer = arr[0]*arr[3];
+		}else if(arr[2] == arr[3]) {
+			answer = arr[0]*arr[1];
+		}else {
+			answer = arr[0];
+		}
+        return answer;
+    }
+}
