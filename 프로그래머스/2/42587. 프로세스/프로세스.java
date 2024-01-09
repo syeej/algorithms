277e8e06617067d730e2247541de036b94e1905c
yoone <141565053+syeej@users.noreply.github.com>
2024-01-09 14:16:48 +0900

    [level 2] Title: 프로세스, Time: 0.65 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,27 @@
+import java.util.PriorityQueue;
+import java.util.Collections;
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        int loc = 0;
+        PriorityQueue<Integer> pque = new PriorityQueue<>(Collections.reverseOrder());
+		for(int num: priorities) {
+			pque.offer(num);
+		}
+        while(!pque.isEmpty()) {
+			
+			for(int i=0; i<priorities.length; i++) {
+				if(priorities[i] == pque.peek()){
+					pque.poll();
+					loc++;
+					if(location == i) {
+						answer = loc;
+						return answer;
+					}
+				}
+			}
+			
+		}
+        return -1;
+    }
+}
