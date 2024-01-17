a353ad63d4d4c42b07dd82d1825db321c1f2d8f9
syJoh <141565053+syeej@users.noreply.github.com>
2024-01-17 10:28:37 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+class Solution {
+    public String[] solution(String[] str_list) {
+        String[] res = {};
+        for(int i=0; i<str_list.length; i++){
+            if(str_list[i].equals("l")){
+                String[] answer = Arrays.copyOfRange(str_list, 0, i);
+                res = answer;
+                break;
+            }else if(str_list[i].equals("r")){
+                String[] answer = Arrays.copyOfRange(str_list, i+1, str_list.length);
+                res = answer;
+                break;
+            }else{
+                
+            }
+        }
+        
+        return res;
+    }
+}
