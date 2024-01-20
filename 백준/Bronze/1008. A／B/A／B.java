542faf4b6ee693d069898961fc310866f38977e2
syJoh <141565053+syeej@users.noreply.github.com>
2024-01-20 22:20:23 +0900

    [Bronze V] Title: A/B, Time: 128 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        String res = Double.toString((double) a /b);
+        bw.write(res);
+        bw.flush();
+        bw.close();
+    }
+}
