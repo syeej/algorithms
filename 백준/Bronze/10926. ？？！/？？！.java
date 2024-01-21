130dce7590dde23ea056c3cea9f3faf32064d305
syJoh <141565053+syeej@users.noreply.github.com>
2024-01-21 20:11:47 +0900

    [Bronze V] Title: ??!, Time: 132 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String str = br.readLine();
+        bw.write(str+"??!");
+        bw.flush();
+        bw.close();
+    }
+}
