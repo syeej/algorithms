aa72def46e4a21e09fe75c4b8fd46ac28ccd1857
syJoh <141565053+syeej@users.noreply.github.com>
2024-01-21 21:34:25 +0900

    [Bronze V] Title: 꼬마 정민, Time: 120 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+
+        bw.write(Long.toString(a+b+c));
+        bw.flush();
+        bw.close();
+    }
+}
