826233e438045fd08b56b78aace35167fc587240
syJoh <141565053+syeej@users.noreply.github.com>
2024-01-23 14:05:01 +0900

    [Bronze V] Title: 개수 세기, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,23 +1,32 @@
 import java.io.*;
+import java.util.*;
 
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int n = Integer.parseInt(br.readLine());
-        String[] arr = br.readLine().split(" ");
-        int num = Integer.parseInt(br.readLine());
+
+        int N = Integer.parseInt(br.readLine());
         int count = 0;
-        
-        for(int i=0; i<n; i++){
-            if(Integer.parseInt(arr[i])==num){
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());;
+
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int b = Integer.parseInt(br.readLine());
+
+        for(int j = 0; j < arr.length; j++){
+            if(b == arr[j]) {
                 count++;
             }
         }
         bw.write(Integer.toString(count));
-        bw.flush();
+
         br.close();
+        bw.flush();
         bw.close();
     }
 }
