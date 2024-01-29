891a3370381ceee4466eb9dc0ebc539333e36a0f
syJoh <141565053+syeej@users.noreply.github.com>
2024-01-29 11:58:05 +0900

    [Bronze II] Title: 숫자의 개수, Time: 144 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -11,31 +11,11 @@ public class Main{
         int c = Integer.parseInt(br.readLine());
         String str = String.valueOf(a*b*c);
         int[] arr = new int[10];
-        String[] strArr = str.split("");
         
-        for(String s: strArr){
-            if(s.equals("0")){
-                arr[0]++;
-            }else if(s.equals("1")){
-                arr[1]++;
-            }else if(s.equals("2")){
-                arr[2]++;
-            }else if(s.equals("3")){
-                arr[3]++;
-            }else if(s.equals("4")){
-                arr[4]++;
-            }else if(s.equals("5")){
-                arr[5]++;
-            }else if(s.equals("6")){
-                arr[6]++;
-            }else if(s.equals("7")){
-                arr[7]++;
-            }else if(s.equals("8")){
-                arr[8]++;
-            }else if(s.equals("9")){
-                arr[9]++;
-            }
+        for(int i=0; i<str.length(); i++){
+            arr[(str.charAt(i)-48)]++;
         }
+        
         for(int num: arr){
             bw.write(num+"\n");
         }
