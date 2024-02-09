9a92d2c54683b55845a78d797a42f4a10b75a142
syJoh <141565053+syeej@users.noreply.github.com>
2024-02-09 19:14:00 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 12000*n + 2000*k;
+        if(n>=10){
+            answer = answer - 2000*(n/10);
+        }
+        return answer;
+    }
+}
