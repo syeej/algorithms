139833e8d03937840bae47aea9379df5779d7083
syJoh <141565053+syeej@users.noreply.github.com>
2024-02-27 15:04:23 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.10 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public long solution(String numbers) {
+        String str = numbers.replace("one", "1")
+            .replace("zero", "0")
+            .replace("two", "2")
+            .replace("three", "3")
+            .replace("four", "4")
+            .replace("five", "5")
+            .replace("six", "6")
+            .replace("seven", "7")
+            .replace("eight", "8")
+            .replace("nine", "9");
+
+        long answer = Long.parseLong(str);
+        return answer;
+    }
+}
