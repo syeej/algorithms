6759a2d78c58d18329ae5a373b98e61b4873f1e3
syJoh <141565053+syeej@users.noreply.github.com>
2024-03-14 18:12:34 +0900

    [level 2] Title: 구명보트, Time: 9.83 ms, Memory: 53.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int idx = 0;
+        
+        for(int i=people.length-1; i>=idx; i--){
+            if(people[i]+people[idx]<=limit){
+                idx++;
+                answer++;
+            }else{
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
