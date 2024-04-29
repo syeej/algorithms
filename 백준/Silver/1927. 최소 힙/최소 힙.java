6de3c8273ca62a827edd669e0d9d661412810b1f
syJoh <141565053+syeej@users.noreply.github.com>
2024-04-29 17:20:06 +0900

    [Silver II] Title: 최소 힙, Time: 324 ms, Memory: 26860 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int n = Integer.parseInt(br.readLine());
+        while(n>0){
+            int x = Integer.parseInt(br.readLine());
+            if(x==0){
+                if(pq.isEmpty()){
+                    bw.write(String.valueOf(0));
+                }else{
+                    bw.write(String.valueOf(pq.poll()));
+                }
+                bw.newLine();
+            }else{
+                pq.offer(x);
+            }
+            n--;
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
