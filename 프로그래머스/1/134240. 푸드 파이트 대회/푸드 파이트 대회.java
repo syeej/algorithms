59b830297407760346d29342bb25d27a323c7f43
syJoh <141565053+syeej@users.noreply.github.com>
2024-04-29 22:49:09 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<food.length; i++){
+          sb.append(String.valueOf(i).repeat(food[i]/2));             
+        }
+        sb.append(String.valueOf(0));
+        for(int i=food.length-1; i>0; i--){
+            sb.append(String.valueOf(i).repeat(food[i]/2));
+        }
+        String answer = sb.toString();
+        return answer;
+    }
+}
