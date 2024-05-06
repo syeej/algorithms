e3b1471a90bd37be3df887f9738c8de7a1014339
syJoh <141565053+syeej@users.noreply.github.com>
2024-05-06 11:49:49 +0900

    [level 2] Title: 기능개발, Time: 0.12 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,35 +1,32 @@
-import java.util.List;
-import java.util.ArrayList;
-
+import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<progresses.length; i++){
+            int job = (100-progresses[i])/speeds[i];
+            if((100-progresses[i])%speeds[i]!=0){
+                job++;
+            }
+            q.offer(job);
+        }
         List<Integer> list = new ArrayList<>();
-		int[] functions = new int[progresses.length];
-		
-		for(int i=0; i<progresses.length; i++) {
-			functions[i] = (100-progresses[i])/speeds[i];
-			if((100-progresses[i])%speeds[i]!=0) {
-				functions[i]+=1;
-			}
-		}
-		
-		int pre = functions[0];
-		int days = 1;
-		for(int i=1; i<progresses.length; i++) {
-			if(pre>=functions[i]) {
-				days++;
-			}else {
-				list.add(days);
-				days = 1;
-				pre = functions[i]; 
-			}
-		}
-		
-		list.add(days);
-		int[] answer = new int[list.size()];
-		for(int i=0; i<list.size(); i++) {
-			answer[i] = list.get(i);
-		}
+        int x = q.poll();
+        int day = 1;
+        while(!q.isEmpty()){
+            if(x >= q.peek()){
+                day++;
+                q.poll();
+            }else{
+                list.add(day);
+                day = 1;
+                x = q.poll();
+            }
+        }
+        list.add(day);
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
+        }
         return answer;
     }
 }
