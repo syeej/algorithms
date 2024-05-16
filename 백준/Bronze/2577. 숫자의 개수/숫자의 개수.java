8fae79db821069701303ce5e8990b2354a192edc
syJoh <141565053+syeej@users.noreply.github.com>
2024-05-16 11:07:33 +0900

    [Bronze II] Title: 숫자의 개수, Time: 120 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,27 +1,20 @@
 import java.io.*;
-import java.util.*;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int a = Integer.parseInt(br.readLine());
         int b = Integer.parseInt(br.readLine());
         int c = Integer.parseInt(br.readLine());
 
-        String[] result = String.valueOf(a*b*c).split("");
+        String[] str = String.valueOf(a*b*c).split("");
         int[] arr = new int[10];
-
-        for(int i=0; i<result.length; i++){
-            arr[Integer.parseInt(result[i])]++;
+        for(int i=0; i<str.length; i++){
+            arr[Integer.parseInt(str[i])]++;
         }
-
         for(int i=0; i<arr.length; i++){
-            bw.write(arr[i]+"\n");
+            System.out.println(arr[i]);
         }
-        bw.flush();
         br.close();
-        bw.close();
     }
 }
