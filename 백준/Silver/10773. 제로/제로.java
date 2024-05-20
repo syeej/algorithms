70106e5b155e9470a9c2bed63d53f9b0648a6bea
syJoh <141565053+syeej@users.noreply.github.com>
2024-05-21 08:27:09 +0900

    [Silver IV] Title: 제로, Time: 236 ms, Memory: 22864 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,30 +1,25 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        Stack<Integer> stack = new Stack<>();
-
         int k = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        int answer = 0;
         while(k>0){
             int num = Integer.parseInt(br.readLine());
-            if(num!=0){
-                stack.push(num);
+            if(num==0){
+                if(!stack.isEmpty()){
+                    answer-=stack.pop();  
+                }
             }else{
-                stack.pop();
+                answer+=num;
+                stack.push(num);
             }
             k--;
         }
-        int sum = 0;
-        for(int n: stack){
-            sum += n;
-        }
-        bw.write(String.valueOf(sum));
-        bw.flush();
         br.close();
-        bw.close();
-
+        System.out.println(answer);
     }
 }
