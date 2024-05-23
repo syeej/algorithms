fa8bed548b836b6c5f8a897d6616bc413f5d8ff1
syJoh <141565053+syeej@users.noreply.github.com>
2024-05-23 09:06:30 +0900

    [level 0] Title: 특수문자 출력하기, Time: 114.65 ms, Memory: 63 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.print("!@#$%^&*(\\\'"+'"'+"<>?:;");
+        /*
+        \\  →  \
+        \'  →  '
+        \"  →  "  
+        */
+    }
+}
