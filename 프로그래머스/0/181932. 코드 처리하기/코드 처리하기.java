9ac518ecc4e96d4de5adcef434367f88604ce772
syJoh <141565053+syeej@users.noreply.github.com>
2024-05-27 08:44:47 +0900

    [level 0] Title: 코드 처리하기, Time: 0.03 ms, Memory: 65.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        int mode = 0;
+		for(int i=0; i<code.length(); i++) {
+			if(mode == 0) {
+				if(code.charAt(i)!= '1') {
+					if(i%2==0) 
+						answer += code.charAt(i);
+				}else if(code.charAt(i) == '1'){
+					mode = 1;
+				}
+			}else if(mode == 1) {
+				if(code.charAt(i) != '1') {
+					if(i%2==1)
+						answer += code.charAt(i);
+				}else if(code.charAt(i) == '1'){
+					mode = 0;
+				}
+			}
+		}
+        if(answer == "")
+            answer = "EMPTY";
+        return answer;
+    }
+}
