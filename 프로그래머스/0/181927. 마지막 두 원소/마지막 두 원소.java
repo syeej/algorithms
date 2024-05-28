064bad78991114182651e4b13d5a95264e7edcc0
syJoh <141565053+syeej@users.noreply.github.com>
2024-05-28 09:54:20 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int len = num_list.length;
+        int[] answer = new int[len+1];
+        for(int i=0; i<num_list.length; i++){
+            answer[i] = num_list[i];
+        }
+        if(num_list[len-1]>num_list[len-2]){
+            answer[len] = num_list[len-1] - num_list[len-2];
+        }else{
+            answer[len] = num_list[len-1]*2;
+        }
+        return answer;
+    }
+}
