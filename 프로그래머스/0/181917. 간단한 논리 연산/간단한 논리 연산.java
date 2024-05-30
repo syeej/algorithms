b1848f07bb452b77399367e6ef37ff399231d94d
syJoh <141565053+syeej@users.noreply.github.com>
2024-05-30 09:33:51 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.02 ms, Memory: 90 MB -BaekjoonHub

---
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        boolean answer = true;
+                   
+        if((x1||x2)&&(x3||x4)){
+            answer = true;
+        }else{
+            answer = false;
+        }
+        return answer;
+    }
+}
