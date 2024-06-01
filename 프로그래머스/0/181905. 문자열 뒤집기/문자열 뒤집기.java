3231257eea5d12672598ce4efda77be7665bbd4a
syJoh <141565053+syeej@users.noreply.github.com>
2024-06-01 09:28:46 +0900

    [level 0] Title: 문자열 뒤집기, Time: 10.41 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        StringBuilder sb = new StringBuilder(my_string.substring(s, e+1));
+        sb.reverse();
+        String answer = my_string.substring(0, s)+sb.toString()+my_string.substring(e+1);
+        return answer;
+    }
+}
