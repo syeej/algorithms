f10989ee7d39d9d3693c47bfae34d3c7f1d6ea35
syJoh <141565053+syeej@users.noreply.github.com>
2024-06-02 00:17:51 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.27 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/README.md
+++ 프로그래머스/2/12941. 최솟값 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최솟값 만들기 - 12941 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12941#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12941) 
 
 ### 성능 요약
 
-메모리: 51.9 MB, 시간: 1.29 ms
+메모리: 52.1 MB, 시간: 1.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 08일 11:24:38
+2024년 06월 02일 00:17:48
 
 ### 문제 설명
 
--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,16 +1,11 @@
-import java.util.*;
 class Solution
 {
     public int solution(int []A, int []B)
     {
         int answer = 0;
 
-        Arrays.sort(A);
-        Arrays.sort(B);
-        
-        for(int i=0; i<A.length; i++){
-            answer += A[i]*B[B.length-i-1];
-        }
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
 
         return answer;
     }
