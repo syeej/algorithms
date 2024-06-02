3405087fc55bb26dd28f58ff3f95369a47c7199e
syJoh <141565053+syeej@users.noreply.github.com>
2024-06-02 13:39:48 +0900

    [level 2] Title: 전화번호 목록, Time: 296.08 ms, Memory: 96 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577#) 
 
 ### 성능 요약
 
-메모리: 97.3 MB, 시간: 371.07 ms
+메모리: 96 MB, 시간: 296.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 4일 18:47:30
+2024년 06월 02일 13:39:46
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,14 +1,24 @@
-import java.util.Arrays;
-
+import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         Arrays.sort(phone_book);
-		for(int i=0; i<phone_book.length-1; i++) {
-			if(phone_book[i+1].startsWith(phone_book[i])) {
-				answer = false;
-			}
-		}
+        /*
+        for(int i=0; i<phone_book.length-1; i++){
+            for(int j=i+1; j<phone_book.length; j++){
+                if(phone_book[j].startsWith(phone_book[i])||phone_book[i].startsWith(phone_book[j])){
+                    answer = false;
+                    break;
+                }
+            }
+        }
+        */
+        for(int i=0; i<phone_book.length-1; i++){
+            if(phone_book[i].startsWith(phone_book[i+1])||phone_book[i+1].startsWith(phone_book[i])){
+                answer = false;
+                break;
+            }
+        }
         return answer;
     }
 }
