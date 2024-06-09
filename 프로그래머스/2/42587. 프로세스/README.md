8ea690964f316937dad57744b843ff65a66a3d97
syJoh <141565053+syeej@users.noreply.github.com>
2024-06-09 22:38:24 +0900

    [level 2] Title: 프로세스, Time: 0.52 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 82.4 MB, 시간: 0.47 ms
+메모리: 84.9 MB, 시간: 0.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 14:25:9
+2024년 06월 09일 22:38:21
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,27 +1,26 @@
-import java.util.PriorityQueue;
-import java.util.Collections;
+import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-        int loc = 0;
+        int loc =0;
         PriorityQueue<Integer> pque = new PriorityQueue<>(Collections.reverseOrder());
-		for(int num: priorities) {
-			pque.offer(num);
-		}
+        for(int num: priorities) {
+            pque.offer(num);
+        }
         while(!pque.isEmpty()) {
-			
-			for(int i=0; i<priorities.length; i++) {
-				if(priorities[i] == pque.peek()){
-					pque.poll();
-					loc++;
-					if(location == i) {
-						answer = loc;
-						return answer;
-					}
-				}
-			}
-			
-		}
-        return answer;
+            for(int i=0; i<priorities.length; i++) {
+                if(priorities[i] == pque.peek()){
+                    //System.out.println(pque.peek()+"  "+loc);
+                    pque.poll();
+                    loc++;
+                    if(location == i) {
+                        answer = loc;
+                        return answer;
+                    }
+                }
+            }
+
+        }
+        return -1;
     }
 }
