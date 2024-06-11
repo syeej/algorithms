4fe9fca59882d3cd416da6d3771630fba9984b2e
syJoh <141565053+syeej@users.noreply.github.com>
2024-06-11 09:31:35 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 8.73 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -2,6 +2,9 @@ class Solution {
     public String[] solution(String my_string) {
         //String[] answer = my_string.trim().split("\\s* \\s*");
         String[] answer = my_string.trim().split("\\s+");
+        // \s는 공백, 탭을 의미
+        //String[] answer = my_string.trim().split("[ ]+"); 
+        //괄호 안의 문자열(여기선 공백) 여러개를 기준으로 나눔
         return answer;
     }
 }
