eae6e85d4aa9060871a8895b16fa79ec15b1cf9f
syJoh <141565053+syeej@users.noreply.github.com>
2024-06-15 17:23:00 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.24 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -1,34 +1,17 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
 class Solution {
     public int solution(int[] rank, boolean[] attendance) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i=0; i<rank.length; i++){
             if(attendance[i]){
-                map.put(i, rank[i]);
+                map.put(rank[i], i); // (등수, 학생 번호) *등수는 모두 서로 다름
             }
         }
-      
-        List<Integer> valueList = new ArrayList<>(map.values());
-        Collections.sort(valueList);
-        int answer = 0;
-        
-        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
-            int key = entry.getKey();
-            int value = entry.getValue();
-            
-            if(value == valueList.get(0)){
-                answer += 10000*key;
-            }else if(value == valueList.get(1)){
-                answer += 100*key;
-            }else if(value == valueList.get(2)){
-                answer += key;
-            }
-        }
+        //Key값(등수)로 오름차순 정렬
+        List<Integer> list = new ArrayList<>(map.keySet());
+        Collections.sort(list);
+        answer = 10000*map.get(list.get(0))+100*map.get(list.get(1))+map.get(list.get(2));
         return answer;
     }
 }
