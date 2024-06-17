07bef5085ac32a55c7f67e8d26a50c91c62dab81
syJoh <141565053+syeej@users.noreply.github.com>
2024-06-17 19:42:34 +0900

    [level 0] Title: 0 떼기, Time: 0.03 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String n_str) {
+ 
+        int num = Integer.parseInt(n_str);
+        String answer = Integer.toString(num);
+        
+        return answer;
+    }
+}
