70486a40e42dfb9f32233db465e5fa632d533451
syJoh <141565053+syeej@users.noreply.github.com>
2024-06-23 10:58:48 +0900

    [Silver V] Title: 방 번호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.py
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -1,18 +1,13 @@
-n = input().strip()
-arr = [0] * 10
-
-for ch in n:
-    num = int(ch)
+word = input()
+ans = [0] * 10
+for i in range(len(word)):
+    num = int(word[i])
     if num == 6 or num == 9:
-        arr[9] += 1
+        if ans[6] <= ans[9]:
+            ans[6] += 1
+        else:
+            ans[9] += 1
     else:
-        arr[num] += 1
-
-# '6'과 '9'는 서로 대체 가능하므로, 둘을 합쳐서 절반으로 나눔
-if arr[9] % 2 == 0:
-    arr[9] //= 2
-else:
-     arr[9] = arr[9] // 2 + 1
-
-answer = max(arr)
-print(answer)
+        ans[num] += 1
+ 
+print(max(ans))
