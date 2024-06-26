6b0dc4a640915a71fe8dd0d0a088a3d9583d8f1a
SungYoonJoh <141565053+syeej@users.noreply.github.com>
2024-06-26 12:56:36 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 604 ms, Memory: 294680 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,35 +1,29 @@
+
 import java.io.*;
 import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException{
+public class Main{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        Queue<Integer> que = new LinkedList<>();
-        for(int i=1; i<=n; i++){
-            que.offer(i);
-        }
         StringBuilder sb = new StringBuilder();
         sb.append("<");
-
-        while(que.size()>1){
-            for(int i=1; i<=k; i++){
-                if(i==k){
-                    sb.append(que.poll()).append(", ");
-                    break;
-                }else{
-                    que.offer(que.poll());
-                }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=1; i<=n; i++){
+            q.add(i);
+        }
+        int idx = 1;
+        while(q.size()>1) {
+            if (idx == k) {
+                sb.append(q.poll()).append(", ");
+                idx = 1;
+            } else {
+                q.add(q.poll());
+                idx++;
             }
         }
-        sb.append(que.poll()+">");
-        bw.write(sb.toString());
-        bw.flush();
-        br.close();
-        bw.close();
+        sb.append(q.poll()).append(">");
+        System.out.println(sb);
     }
 }
