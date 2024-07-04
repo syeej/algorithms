e2449e6ece6ed54430c0047251767c8ea7c62e1b
SungYoonJoh <141565053+syeej@users.noreply.github.com>
2024-07-04 17:59:14 +0900

    [Silver III] Title: 회전하는 큐, Time: 128 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -1,48 +1,38 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException{
+class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        LinkedList<Integer> deque = new LinkedList<>();
-        for(int i=1; i<=n; i++)
-            deque.offer(i);
-
         int m = Integer.parseInt(st.nextToken());
-        int[] arr = new int[m];
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<m; i++)
-            arr[i] = Integer.parseInt(st.nextToken());
-        int count = 0;
-        /*세팅 끝*/
-        for(int i=0; i<arr.length; i++){
-            int targetIdx = deque.indexOf(arr[i]);
-            int halfIdx = deque.size()/2;
-            if(deque.size()%2==0){
-                halfIdx = deque.size()/2-1;
-            }
-            if(targetIdx <= halfIdx){ //2번 연산
-                for(int j=0; j<targetIdx; j++){
-                    int tmp = deque.pollFirst();
-                    deque.offerLast(tmp);
-                    count++;
-                }
-            }else{ //3번 연산
-                for(int j=0; j<deque.size()-targetIdx; j++){
-                    int tmp = deque.pollLast();
-                    deque.offerFirst(tmp);
-                    count++;
-                }
-            }
-            deque.pollFirst();
+        LinkedList<Integer> q = new LinkedList<>();
+        for(int i=1; i<=n; i++){
+            q.add(i);
         }
-        bw.write(String.valueOf(count));
-        bw.flush();
+        int[] numList = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<m; i++){
+            numList[i] = Integer.parseInt(st.nextToken());
+        }
+        int count=0;
+        for(int num: numList){
+            int idx = q.indexOf(num);
+            if(idx<=q.size()/2){
+                for(int i=0; i<idx; i++){
+                    q.offerLast(q.pollFirst());
+                    count++;
+                }
+            }else{
+                for(int i=0; i<q.size()-idx; i++){
+                    q.offerFirst(q.pollLast());
+                    count++;
+                }
+            }
+            q.pollFirst();
+        }
         br.close();
-        bw.close();
+        System.out.println(count);
     }
 }
