5b3a29efb46ef83337dfed68f820d0ac147bee5c
SungYoonJoh <141565053+syeej@users.noreply.github.com>
2024-07-07 22:18:42 +0900

    [level 1] Title: 옹알이 (2), Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -5,6 +5,6 @@ def solution(babbling):
         for word in words:
             if word*2 not in b:
                 b = b.replace(word, ' ')
-        if b.isspace():
+        if len(b.strip())==0:
             answer += 1
     return answer
