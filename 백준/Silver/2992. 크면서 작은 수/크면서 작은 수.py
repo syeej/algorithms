4e8151b429e29c87a2f1203538c3a80de03c6a60
SungYoonJoh <141565053+syeej@users.noreply.github.com>
2024-07-09 01:59:43 +0900

    [Silver III] Title: 크면서 작은 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.py
+++ 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.py
@@ -2,15 +2,19 @@ import sys
 from itertools import permutations
 
 input = sys.stdin.readline
-x = input().strip()
-arr = list(permutations(x, len(x)))
+n = input().strip()
+#입력된 문자열의 모든 순열
+r = list(permutations(n, len(n)))
 nums=[]
-for i in arr:
-    num = int(''.join(i))
-    if num> int(x) and num not in nums:
+
+for i in r:
+    num = int(''.join(i)) #순열 -> 정수 변환
+    #n보다 큰 수면서 중복이 아닌 경우
+    if num> int(n) and num not in nums:
         nums.append(num)
-        nums.sort()
+
 if nums:
-    print(nums[0])
+  nums.sort()
+  print(nums[0])
 else:
-    print(0)
+  print(0)
