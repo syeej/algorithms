42fc96b8cc0e966db1403846a4de2a0fcf44da62
SungYoonJoh <141565053+syeej@users.noreply.github.com>
2024-07-27 17:05:35 +0900

    [Silver II] Title: 나무 자르기, Time: 4552 ms, Memory: 143264 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -2,7 +2,8 @@ import sys
 input = sys.stdin.readline
 n, m = map(int, input().split())
 trees = list(map(int, input().split()))
-start, end = 1, max(trees)
+#start, end = min(trees), max(trees)
+start, end = 1, max(trees) #나무중 가장 짧은 길이 1
 
 while start<=end:
     mid = (start+end)//2
@@ -10,7 +11,7 @@ while start<=end:
     for tree in trees:
         if tree>=mid:
             sum += (tree-mid)
-    if sum >= m:
+    if sum >= m: # 목표(m) 이상일 경우
         start = mid+1
     else:
         end = mid-1
