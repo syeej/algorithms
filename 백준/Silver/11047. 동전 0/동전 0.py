1bff843457df17799c29c354c3394445061fecef
SungYoonJoh <141565053+syeej@users.noreply.github.com>
2024-08-02 13:55:29 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+n, k = map(int, input().split())
+coins = []
+for _ in range(n):
+    coins.append(int(input().strip()))
+# coins.sort(reverse=True)
+answer =0
+for i in range(n-1, -1, -1):
+    answer += k // coins[i]
+    k %= coins[i]
+print(answer)
