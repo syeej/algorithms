e617a72b78c28ac7dd94114c68af2dbdce7050c4
SungYoonJoh <141565053+syeej@users.noreply.github.com>
2024-08-04 09:45:04 +0900

    [Silver III] Title: 게임, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.py
@@ -0,0 +1,17 @@
+# 승률(z)이 기준
+x, y = map(int, input().split())
+z = 100*y//x
+start, end = 1, x # x의 범위
+count = 0
+#승률이 더 오르지 않을 때는 a현재 승률이 99이상
+if z>=99:
+    print(-1)
+else:
+    while start<=end:
+        mid = (start+end)//2
+        if (y+mid)*100//(x+mid) <= z:
+            start = mid+1
+        else:
+            count = mid
+            end = mid-1
+    print(count)
