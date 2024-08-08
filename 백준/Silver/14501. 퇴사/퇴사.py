68e2ca12fba09c6928c61e5c855f4b424ef2fa76
SungYoonJoh <141565053+syeej@users.noreply.github.com>
2024-08-08 19:58:17 +0900

    [Silver III] Title: 퇴사, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -9,10 +9,19 @@ for _ in range(n):
     tp.append([t, p])
 """
 dp = [0 for _ in range(n+1)]
+"""
+# Top-Down
 for i in range(n-1, -1, -1):
-  if i+tp[i][0] > n: #i일에 상담할 때 퇴사일을 넘기면 상담 안함
+  if i+tp[i][0] > n: #i일에 상담하면 퇴사일을 넘기면 상담 안함
     dp[i] = dp[i+1]
   else:
+    # i일에 상담 vs 상담 안하는 것 중 큰 값 선택
     dp[i] = max(dp[i+1], tp[i][1] + dp[i+tp[i][0]])
-
 print(dp[0])
+"""
+#Bottom-up
+for i in range(n):
+    for j in range(i+tp[i][0], n+1):
+        if dp[j] < dp[i]+tp[i][1]:
+            dp[j] = dp[i] + tp[i][1]
+print(dp[-1])
