d40b03c7f44b00f57b14af2510418807d4f18bc5
SungYoonJoh <141565053+syeej@users.noreply.github.com>
2024-08-14 23:52:47 +0900

    [Gold V] Title: 카드 바꾸기, Time: 392 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/25401. 카드 바꾸기/카드 바꾸기.py
+++ 백준/Gold/25401. 카드 바꾸기/카드 바꾸기.py
@@ -1,22 +1,22 @@
-N = int(input())
+from sys import stdin
 
-arr = [0] + list(map(int, input().split()))
+n = int(stdin.readline())
+card = list(map(int, stdin.readline().split()))
+pair = set()
 
-def check(s, diff):
-    cnt = 0
-    for i in range(1, N + 1):
-        s += diff
-        if arr[i] != s:
-            cnt += 1
-    return cnt
+for i in range(n - 1):
+    for j in range(i + 1, n):
+        a = (card[j] - card[i]) / (j - i) # 가능한 모든 기울기(n-1개)
+        if a - int(a) == 0: # 기울기가 정수가 나오지 않으면 탈락(모든 카드가 정수이기 때문)
+            b = card[j] - a * j # a가 정해졌을 때 나오는 b
+            pair.add((a, b))
 
-ans = 500
-for i in range(1, N):
-    for j in range(i + 1, N + 1):
-        diff = (arr[j] - arr[i]) / (j - i)
+change_list = []
+for (a, b) in pair: # 가능한 모든 (a, b) 쌍으로 그래프를 만들고 카드를 대입
+    change = 0
+    for l in range(n):
+        if card[l] != a * l + b:
+            change += 1
+    change_list.append(change)
 
-        if diff - int(diff) != 0:
-            continue
-
-        ans = min(ans, check(arr[i] - diff * i, diff))
-print(ans)
+print(min(change_list)) # 바꿔야 하는 카드 개수의 최소값 출력
