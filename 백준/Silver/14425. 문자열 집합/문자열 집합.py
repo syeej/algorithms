1b6ab899387712b4fde78828cb850c640aa1e3f2
SungYoonJoh <141565053+syeej@users.noreply.github.com>
2024-09-09 07:41:49 +0900

    [Silver IV] Title: 문자열 집합, Time: 80 ms, Memory: 36380 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,11 +1,26 @@
+"""
+36120KB / 3860 ms
 import sys
-
 input = sys.stdin.readline
 n, m = map(int, input().split())
 arr = []
 for _ in range(n):
     word = input().strip()
     arr.append(word)
+answer = 0
+for _ in range(m):
+    k = input().strip()
+    if k in arr:
+        answer+=1
+print(answer)
+"""
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+arr = dict()
+for _ in range(n):
+    word = input().strip()
+    arr[word] = 1
 answer = 0
 for _ in range(m):
     k = input().strip()
