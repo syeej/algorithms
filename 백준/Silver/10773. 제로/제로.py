864748301326830b8503d42f104476e1d3fdb8ba
SungYoonJoh <141565053+syeej@users.noreply.github.com>
2024-09-25 21:28:08 +0900

    [Silver IV] Title: 제로, Time: 72 ms, Memory: 31900 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,12 +1,16 @@
 import sys
-k = int(sys.stdin.readline())
+# from collections import deque
+input = sys.stdin.readline
+
+k = int(input())
+# dq = deque()
 arr = []
 answer = 0
 for _ in range(k):
-    num = int(sys.stdin.readline())
-    if num!=0:
-        arr.append(num)
-        answer += num
-    else:
+    num = int(input())
+    if num == 0:
         answer -= arr.pop()
+    else:
+        answer += num
+        arr.append(num)
 print(answer)
