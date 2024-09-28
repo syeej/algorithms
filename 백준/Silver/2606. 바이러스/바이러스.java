bdae7162d4dad2e14378f8eb20444cf1ae3dc522
SungYoonJoh <141565053+syeej@users.noreply.github.com>
2024-09-28 17:03:17 +0900

    [Silver III] Title: 바이러스, Time: 96 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,37 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static  int[][] computers;
-    static boolean[] visited;
-
+    static int[][] computers;//컴퓨터 간 연결 상태 저장
+    static boolean[] checked; //컴퓨터 감염 여부 체크
     static int n, count;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        //컴퓨터 개수
         n = Integer.parseInt(br.readLine());
-        //직접 연결된 컴퓨터 쌍의 수
         int m = Integer.parseInt(br.readLine());
-
-        computers = new int[n+1][n+1]; //1번컴퓨터부터 시작
-        visited = new boolean[n+1];
-
+        
+        computers = new int[n+1][n+1]; //1부터 시작
+        checked = new boolean[n+1];
         for(int i=0; i<m; i++){
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             computers[x][y] = computers[y][x] = 1;
         }
-
         dfs(1);
-        System.out.println(count);
         br.close();
+        System.out.println(count);
     }
-    public static void dfs(int x){
-        visited[x] = true; //방문 처리
+
+    static void dfs(int x){
+        checked[x] = true; //감염
         for(int i=1; i<n+1; i++){
-            if(!visited[i] && computers[x][i]==1){
+            // 1~n 까지 컴퓨터 반복
+            // i번 컴퓨터가 아직 감염 안 됐고, x와 연결되어 있을 때
+            if(!checked[i] && computers[x][i]==1){
                 dfs(i);
                 count++;
             }
