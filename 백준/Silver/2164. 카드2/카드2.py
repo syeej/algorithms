f06f179c0fbcfbbe541c5bd0f6540f6547fcb06f
SungYoonJoh <141565053+syeej@users.noreply.github.com>
2024-11-03 20:28:21 +0900

    [Silver IV] Title: 카드2, Time: 220 ms, Memory: 50972 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,28 +1,9 @@
 from collections import deque
 n = int(input())
-"""
-arr = deque()
-for i in range(n):
-    arr.append(i+1)
-"""
-arr = deque(i for i in range(1, n+1))
-while len(arr)>1:
-    arr.popleft()
-    arr.append(arr.popleft())
 
-print(arr[0])
+dq = deque(i for i in range(1, n+1))
+while len(dq) > 1:
+    dq.popleft()
+    dq.append(dq.popleft())
 
-"""
-# 시간촐과
-import sys
-n = int(sys.stdin.readline())
-arr = []
-for i in range(n):
-    arr.append(i+1)
-
-while len(arr)>1:
-    arr.pop(0)
-    arr.append(arr.pop(0))
-
-print(arr.pop(0))
-"""
+print(dq[0])
