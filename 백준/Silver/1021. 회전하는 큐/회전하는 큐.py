314e4d7845b7e5866a148f04cbb600b06290b3f7
SungYoonJoh <141565053+syeej@users.noreply.github.com>
2024-11-24 21:32:59 +0900

    [Silver III] Title: 회전하는 큐, Time: 56 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -2,21 +2,17 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 n, m = map(int, input().split())
-num_list = list(map(int, input().split()))
-q = deque(range(1, n+1))
+arr = list(map(int, input().split()))
+dq = deque(range(1, n+1))
 count = 0
 
-#search
-for num in num_list:
-    if q[0] == num: #원하는 값 나오면 pop
-        q.popleft()
+for num in arr:
+    idx = dq.index(num)
+    if idx <= len(dq)//2:
+        dq.rotate(-idx)
+        count += idx
     else:
-        idx = q.index(num)
-        if idx <= len(q)//2:
-            q.rotate(-idx)
-            count += idx
-        else:
-            q.rotate(len(q)-idx)
-            count += len(q) - idx
-        q.popleft()
+        dq.rotate(len(dq)-idx)
+        count += len(dq) - idx
+    dq.popleft()
 print(count)
